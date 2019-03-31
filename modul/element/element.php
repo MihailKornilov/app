2197edec377decaafff88ecf57cf34bdcf10376e
miha <arunta-server@yandex.ru>
2019-03-31 15:25:56 +0300

    [5] доработка 1

--- modul/element/element.php
+++ modul/element/element.php
@@ -428,16 +428,19 @@ function _element5_struct($el) {
 		'req'       => _num($el['req']),
 		'req_msg'   => $el['req_msg'],
 
-		'txt_1' => $el['txt_1'] //текст для placeholder
+		'txt_1' => $el['txt_1'],//текст для placeholder
+		'txt_2' => $el['txt_2'] //начальный текст
 	) + _elementStruct($el);
 }
 function _element5_print($el, $prm) {
 	$placeholder = $el['txt_1'] ? ' placeholder="'.$el['txt_1'].'"' : '';
 	$disabled = $prm['blk_setup'] ? ' disabled' : '';
 
+	$v = _elemPrintV($el, $prm, $el['txt_2']);
+
 	return
 	'<textarea id="'._elemAttrId($el, $prm).'"'._elemStyleWidth($el).$placeholder.$disabled.'>'.
-		_elemPrintV($el, $prm).
+		$v.
 	'</textarea>';
 }
 function _element5_print11($el, $u) {
@@ -1914,7 +1917,7 @@ function _element36_struct_title($el) {
 	return $el;
 }
 function _element36_print($el) {
-	$type = PHP12_icon18_type($el['num_1']);
+	$type = _element36type($el['num_1']);
 	$pl = _dn(!$el['num_2'], 'pl');
 	$cur = $el['num_3'] ? ' curP' : ' curD';
 
@@ -1926,6 +1929,75 @@ function _element36TT($el) {
 
 	return _tooltip($el['txt_1'], -10, 'l');
 }
+function _element36type($id='all') {//доступные варианты иконок
+	$icon = array(
+		1 => 'hint',
+		2 => 'print',
+		3 => 'ok',
+		4 => 'set',
+		5 => 'set-b',
+		6 => 'client',
+		7 => 'worker',
+		8 => 'vk',
+		9 => 'rub',
+		10 => 'usd',
+		11 => 'stat',
+		12 => 'set-dot',
+		13 => 'info',
+		14 => 'search',
+		15 => 'star',
+		16 => 'comment',
+		17 => 'add',
+		18 => 'edit',
+		19 => 'del',
+		20 => 'del-red',
+		21 => 'doc-add',
+		22 => 'order',
+		23 => 'calendar',
+		24 => 'eye',
+		25 => 'clock',
+		26 => 'cancel',
+		27 => 'recover',
+		28 => 'off',
+		29 => 'offf',
+		30 => 'out',
+		31 => 'chain',
+		32 => 'move',
+		33 => 'move-x',
+		34 => 'move-y',
+		35 => 'sub',
+		36 => 'join',
+		37 => 'cube',
+		38 => 'filter',
+		39 => 'link',
+		40 => 'hand'
+	);
+
+	if($id == 'all')
+		return $icon;
+
+	return isset($icon[$id]) ? $icon[$id] : 'empty';
+}
+function PHP12_icon18_list($prm) {
+	$sel = 0;
+	if($col = $prm['el12']['col'])
+		if($u = $prm['unit_edit'])
+			$sel = $u[$col];
+
+	$send = '';
+	foreach(_element36type() as $id => $name) {
+		$send .=
+			'<div class="icu over1'._dn($id!=$sel, 'sel').'" val="'.$id.'">'.
+				'<div class="icon icon-'.$name.' curP"></div>'.
+			'</div>';
+	}
+
+	return
+	'<div class="_icon-choose mt3">'.
+		$send.
+	'</div>';
+}
+
 
 /* [37] Select: выбор колонки таблицы (SA) */
 function _element37_struct($el) {
@@ -6802,75 +6874,6 @@ function PHP12_balans_setup_vvv($prm) {
 
 
 
-/* ---=== ВЫБОР ИКОНКИ [36] ===--- */
-function PHP12_icon18_list($prm) {
-	$sel = 0;
-	if($col = $prm['el12']['col'])
-		if($u = $prm['unit_edit'])
-			$sel = $u[$col];
-
-	$send = '';
-	foreach(PHP12_icon18_type() as $id => $name) {
-		$send .=
-			'<div class="icu over1'._dn($id!=$sel, 'sel').'" val="'.$id.'">'.
-				'<div class="icon icon-'.$name.' curP"></div>'.
-			'</div>';
-	}
-
-	return
-	'<div class="_icon-choose mt3">'.
-		$send.
-	'</div>';
-}
-function PHP12_icon18_type($id='all') {//доступные варианты иконок
-	$icon = array(
-		1 => 'hint',
-		2 => 'print',
-		3 => 'ok',
-		4 => 'set',
-		5 => 'set-b',
-		6 => 'client',
-		7 => 'worker',
-		8 => 'vk',
-		9 => 'rub',
-		10 => 'usd',
-		11 => 'stat',
-		12 => 'set-dot',
-		13 => 'info',
-		14 => 'search',
-		15 => 'star',
-		16 => 'comment',
-		17 => 'add',
-		18 => 'edit',
-		19 => 'del',
-		20 => 'del-red',
-		21 => 'doc-add',
-		22 => 'order',
-		23 => 'calendar',
-		24 => 'eye',
-		25 => 'clock',
-		26 => 'cancel',
-		27 => 'recover',
-		28 => 'off',
-		29 => 'offf',
-		30 => 'out',
-		31 => 'chain',
-		32 => 'move',
-		33 => 'move-x',
-		34 => 'move-y',
-		35 => 'sub',
-		36 => 'join',
-		37 => 'cube',
-		38 => 'filter',
-		39 => 'link',
-		40 => 'hand'
-	);
-
-	if($id == 'all')
-		return $icon;
-
-	return isset($icon[$id]) ? $icon[$id] : 'empty';
-}
 
 
 
