33ed2854182ee2ee7aa7a9a615d24b0796302066
miha <arunta-server@yandex.ru>
2019-06-24 15:52:50 +0300

    [36] перенос в отдельный файл

--- modul/element/element.php
+++ modul/element/element.php
@@ -22,7 +22,7 @@ foreach(array(
 			 1, 2, 3, 4, 5, 6, 7, 8, 9,10,
 			11,12,   14,
 
-	                          37,
+	                       36,37,
 			                           90
         ) as $id) {
 	$file = GLOBAL_DIR.'/modul/element/element'.$id.'.php';
@@ -1411,101 +1411,6 @@ function _element35_vvv($el) {
 	return json_decode($el['txt_1']);
 }
 
-/* [36] Иконка */
-function _element36_struct($el) {
-	return array(
-		'num_1'   => _num($el['num_1']),//id иконки
-		'num_2'   => _num($el['num_2']),//изменять яркость при наведении мышкой
-		'num_3'   => _num($el['num_3']),//курсор рука при наведении, иначе стрелочка
-		'txt_1' => $el['txt_1']         //подсказка
-	) + _elementStruct($el);
-}
-function _element36_struct_title($el) {
-	$el['title'] = 'ICON';
-	return $el;
-}
-function _element36_print($el) {
-	$type = _element36type($el['num_1']);
-	$pl = _dn(!$el['num_2'], 'pl');
-	$cur = $el['num_3'] ? ' curP' : ' curD';
-
-	return '<div class="icon icon-'.$type.$pl.$cur._element36TT($el).'</div>';
-}
-function _element36TT($el) {
-	if(empty($el['txt_1']))
-		return '">';
-
-	return _tooltip($el['txt_1'], -10, 'l');
-}
-function _element36type($id='all') {//доступные варианты иконок
-	$icon = array(
-		1 => 'hint',
-		2 => 'print',
-		3 => 'ok',
-		4 => 'set',
-		5 => 'set-b',
-		6 => 'client',
-		7 => 'worker',
-		8 => 'vk',
-		9 => 'rub',
-		10 => 'usd',
-		11 => 'stat',
-		12 => 'set-dot',
-		13 => 'info',
-		14 => 'search',
-		15 => 'star',
-		16 => 'comment',
-		17 => 'add',
-		18 => 'edit',
-		19 => 'del',
-		20 => 'del-red',
-		21 => 'doc-add',
-		22 => 'order',
-		23 => 'calendar',
-		24 => 'eye',
-		25 => 'clock',
-		26 => 'cancel',
-		27 => 'recover',
-		28 => 'off',
-		29 => 'offf',
-		30 => 'out',
-		31 => 'chain',
-		32 => 'move',
-		33 => 'move-x',
-		34 => 'move-y',
-		35 => 'sub',
-		36 => 'join',
-		37 => 'cube',
-		38 => 'filter',
-		39 => 'link',
-		40 => 'hand'
-	);
-
-	if($id == 'all')
-		return $icon;
-
-	return isset($icon[$id]) ? $icon[$id] : 'empty';
-}
-function PHP12_icon18_list($prm) {
-	$sel = 0;
-	if($col = $prm['el12']['col'])
-		if($u = $prm['unit_edit'])
-			$sel = $u[$col];
-
-	$send = '';
-	foreach(_element36type() as $id => $name) {
-		$send .=
-			'<div class="icu over1'._dn($id!=$sel, 'sel').'" val="'.$id.'">'.
-				'<div class="icon icon-'.$name.' curP"></div>'.
-			'</div>';
-	}
-
-	return
-	'<div class="_icon-choose mt3">'.
-		$send.
-	'</div>';
-}
-
 /* [38] Select: выбор диалогового окна (SA) */
 function _element38_struct($el) {
 	return array(
@@ -2596,21 +2501,21 @@ function _element78_js($el) {
 }
 function _element78_print($el) {
 	if(!$ids = _ids($el['txt_1'], 1))
-		return _emptyMin10('Фильтр-меню: отсутствуют ID значений.');
+		return _emptyMin('Фильтр-меню: отсутствуют ID значений.');
 
 	$c = count($ids) - 1;
 	$elem_id = $ids[$c];
 
 	if(!$EL = _elemOne($elem_id))
-		return _emptyMin10('Фильтр-меню: значение отсутствует.');
+		return _emptyMin('Фильтр-меню: значение отсутствует.');
 	if(!$BL = $EL['block'])
-		return _emptyMin10('Фильтр-меню: нет блока.');
+		return _emptyMin('Фильтр-меню: нет блока.');
 	if($BL['obj_name'] != 'dialog')
-		return _emptyMin10('Фильтр-меню: блок не из диалога.');
+		return _emptyMin('Фильтр-меню: блок не из диалога.');
 	if(!$dialog_id = $BL['obj_id'])
-		return _emptyMin10('Фильтр-меню: нет ID диалога.');
+		return _emptyMin('Фильтр-меню: нет ID диалога.');
 	if(!$dialog = _dialogQuery($dialog_id))
-		return _emptyMin10('Фильтр-меню: нет диалога.');
+		return _emptyMin('Фильтр-меню: нет диалога.');
 
 	$col = $EL['col'];//колонка текстового значения
 	$colCount = '';//колонка значения количества
@@ -2631,7 +2536,7 @@ function _element78_print($el) {
 			WHERE ".$cond."
 			ORDER BY `sort`,`id`";
 	if(!$arr = query_arr($sql))
-		return _emptyMin10('Фильтр-меню: пустое меню.');
+		return _emptyMin('Фильтр-меню: пустое меню.');
 
 	$send = '';
 	$v = _spisokFilter('vv', $el, 0);
