ed9bba2d35faff0057388cfec238ad89dfbdf926
miha <arunta-server@yandex.ru>
2019-06-30 19:55:53 +0300

    [70] перенос в отдельный файл

--- modul/element/element.php
+++ modul/element/element.php
@@ -24,6 +24,7 @@ foreach(array(
 
 	                       36,37,
 			   52,
+			                           70,
 			                           90
         ) as $id) {
 	$file = GLOBAL_DIR.'/modul/element/element'.$id.'.php';
@@ -2233,59 +2234,6 @@ function _element69_print($el, $prm) {
 	}
 
 	return $us['i'].' '.$us['f'];
-}
-
-/* [70] Выбор цвета фона */
-function _element70_struct($el) {
-	return _elementStruct($el);
-}
-function _element70_print($el, $prm) {
-	$v = _elemPrintV($el, $prm, '#fff');
-
-	return '<input type="hidden" id="'._elemAttrId($el, $prm).'" value="'.$v.'" />'.
-		   '<div class="_color-bg" style="background-color:'.$v.'"></div>';
-}
-function _element70_vvv($el, $prm) {
-	$color = array(
-		'#fff',
-		'#ffffe4',
-		'#e4ffe4',
-		'#dff',
-		'#ffe8ff',
-
-		'#f9f9f9',
-		'#ffb',
-		'#cfc',
-		'#aff',
-		'#fcf',
-
-		'#f3f3f3',
-		'#fec',
-		'#F2F2B6',
-		'#D7EBFF',
-		'#ffe4e4',
-
-		'#ededed',
-		'#FFDA8F',
-		'#E3E3AA',
-		'#B2D9FF',
-		'#fcc'
-	);
-
-	$sel = '#fff';//выбранное значение
-	if($u = $prm['unit_edit']) {
-		$col = $el['col'];
-		$sel = $u[$col];
-	}
-
-	$spisok = '';
-	for($n = 0; $n < count($color); $n++) {
-		$cls = $sel == $color[$n] ? ' class="sel"' : '';
-		$spisok .= '<div'.$cls.' style="background-color:'.$color[$n].'" val="'.$color[$n].'">'.
-						'&#10004;'.
-				   '</div>';
-	}
-	return '<div class="_color-bg-choose">'.$spisok.'</div>';
 }
 
 /* [71] Значение записи: иконка сортировки */
