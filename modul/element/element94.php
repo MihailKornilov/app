3a64364cdebb48adeeb9065cf369742df947019e
miha <arunta-server@yandex.ru>
2019-09-06 14:37:15 +0300

    подсказка для блока - 4

--- modul/element/element94.php
+++ modul/element/element94.php
@@ -1,44 +1,47 @@
 <?php
 
-/* [93] Количество новых записей за период */
-function _element93_struct($el) {
+/* [94] Сумма новых записей за период */
+function _element94_struct($el) {
 	return array(
 		'num_1'   => _num($el['num_1']),//список
-		'num_2'   => _num($el['num_2']) /*период:
+		'num_2'   => _num($el['num_2']),//элемент-сумма [13]
+		'num_3'   => _num($el['num_3']) /*период:
 												0 - всё время
-											11137 - сегодня
-											11138 - текущая неделя
-											11139 - текущий месяц
-											11140 - текущий год
+											11172 - сегодня
+											11173 - текущая неделя
+											11174 - текущий месяц
+											11175 - текущий год
 										*/
 	) + _elementStruct($el);
 }
-function _element93_print($el) {
+function _element94_print($el) {
 	if(!$DLG = _dialogQuery($el['num_1']))
-		return _msgRed('[93] не указан список');
+		return _msgRed('[94] не указан список');
+	if(!$colSum = _elemCol($el['num_2']))
+		return _msgRed('[94] не указан элемент-сумма');
 
-	$sql = "SELECT COUNT(*)
+	$sql = "SELECT SUM(`".$colSum."`)
 			FROM  "._queryFrom($DLG)."
 			WHERE "._queryWhere($DLG);
 
-	switch($el['num_2']) {
+	switch($el['num_3']) {
 		//сегодня
-		case 11137:
+		case 11172:
 			$sql .= " AND DATE_FORMAT(`dtime_add`,'%Y-%m-%d')=DATE_FORMAT(CURRENT_TIMESTAMP,'%Y-%m-%d')";
 			break;
 		//текущая неделя
-		case 11138:
+		case 11173:
 			$sql .= " AND DATE_FORMAT(`dtime_add`,'%u')=DATE_FORMAT(CURRENT_TIMESTAMP,'%u') AND `dtime_add`>DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)";
 			break;
 		//текущий месяц
-		case 11139:
+		case 11174:
 			$sql .= " AND DATE_FORMAT(`dtime_add`,'%Y-%m')=DATE_FORMAT(CURRENT_TIMESTAMP,'%Y-%m')";
 			break;
 		//текущий год
-		case 11140:
+		case 11175:
 			$sql .= " AND DATE_FORMAT(`dtime_add`,'%Y')=DATE_FORMAT(CURRENT_TIMESTAMP,'%Y')";
 			break;
 	}
 
-	return _num(query_value($sql));
+	return _sumSpace(query_value($sql));
 }
