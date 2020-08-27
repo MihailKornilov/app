1d497f8129a05fd9b6a41c5d63ef87ba8228327b
miha <arunta-server@yandex.ru>
2020-08-27 11:31:30 +0300

    fix 2020-08-27:1

--- modul/element/element4.php
+++ modul/element/element4.php
@@ -6,6 +6,9 @@ function _element4_struct($el) {
 		'txt_1' => $el['txt_1'] //текст заголовка
 	) + _elementStruct($el);
 }
+function _element4_title($el) {
+	return $el['txt_1'];
+}
 function _element4_print($el) {
 	return '<div class="hd2">'.$el['txt_1'].'</div>';
 }
