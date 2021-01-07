ce616fb5d316f2147bcf1dfa721026ae1d0492a8
miha <arunta-server@yandex.ru>
2021-01-07 16:36:31 +0300

    Перенос Купца 2021-01-07:1

--- 3495523_kupez.php
+++ 3495523_kupez.php
@@ -3,7 +3,7 @@
 	Комтекс: 3495523 -> 4
 
 	ОСОБЕННОСТИ ПЕРЕНОСА:
-		1.
+		1. Внесения и выводы денег убраны (перенесены в переводы между счетами)
 */
 
 function _elem129_kupez($DLG, $POST_CMP) {
@@ -28,15 +28,21 @@ function _elem129_kupez($DLG, $POST_CMP) {
 
 			_kupez_rubric();
 			_kupez_zayav_ob();
+			_kupez_zayav_rek();
+			_kupez_zayav_rek_image();
+			_kupez_zayav_poz();
+			_kupez_zayav_poz_image();
 
 			_kupez_invoice();
+			_kupez_invoice_transfer();
+			_kupez_invoice_in_out();
 			_kupez_expense_category();
 			_kupez_expense();
 
 			break;
 		//частичный
 		case 2:
-			_kupez_expense();
+			_kupez_invoice_in_out();
 			break;
 
 		default:
@@ -318,6 +324,263 @@ function _kupez_zayav_ob() {//заявки-объявления
 		query($sql);
 	}
 }
+function _kupez_zayav_rek() {//заявки-реклама
+	$dialog_id = _comtexSpisokClear(1486);
+
+	_db2();
+	$sql = "SELECT *
+			FROM _zayav
+			WHERE `app_id`=".APP_ID_OLD."
+			  AND `service_id`=9
+			ORDER BY `id`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	$mass = array();
+	foreach($arr as $zayav_id => $r) {
+		$mass[] = "(
+				".$zayav_id.",
+				".APP_ID.",
+				".$r['nomer'].",
+				".$dialog_id.",
+				
+				"._comtexAss(1040, $r['client_id']).",/* клиент */
+
+				".$r['size_x'].",
+				".$r['size_y'].",
+				".round($r['size_x']*$r['size_y']).",
+				".$r['skidka'].",
+
+				".$r['sum_manual'].",
+				".$r['sum_cost'].",
+
+				"._comtexUserId($r).",
+				'".$r['dtime_add']."',
+				".$r['deleted']."
+			)";
+	}
+
+	$sql = "INSERT INTO `_spisok` (
+				  `id_old`,
+				  `app_id`,
+				  `num`,
+				  `dialog_id`,
+				
+				  num_1,
+
+				  sum_2,
+				  sum_3,
+				  sum_4,
+				  num_2,
+
+				  num_3,
+				  sum_1,
+
+				  user_id_add,
+				  dtime_add,
+				  deleted
+			) VALUES ".implode(',', $mass);
+	query($sql);
+
+	_comtexErrMsg($dialog_id, 'num_1', 'Клиенты');
+}
+function _kupez_zayav_rek_image() {//изображения к рекламе
+	$sql = "DELETE FROM `_image` WHERE `app_id`=".APP_ID;
+	query($sql);
+
+	$sql = "UPDATE `_spisok`
+			SET `txt_1`=''
+			WHERE `dialog_id`=1486";
+	query($sql);
+
+	_db2();
+	$sql = "SELECT *
+			FROM `_image`
+			WHERE `app_id`=".APP_ID_OLD."
+			  AND !`deleted`
+			  AND `unit_name`='zayav'
+			  AND `unit_id` IN (
+			    SELECT `id` FROM `_zayav` WHERE `service_id`=9
+			  )
+			ORDER BY `unit_id`,`sort`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	$ASS = array();
+	foreach($arr as $r) {
+		if(!$zayav_id = _comtexAss(1486, $r['unit_id']))
+			continue;
+		if(!isset($ASS[$zayav_id]))
+			$ASS[$zayav_id] = array();
+
+		$sql = "INSERT INTO `_image` (
+					`app_id`,
+	
+					server_id,
+					max_name,
+					max_x,
+					max_y,
+					max_size,
+					80_name,
+					80_x,
+					80_y,
+					80_size,
+	
+					sort,
+					user_id_add,
+					dtime_add
+				) VALUES (
+					".APP_ID.",
+
+					"._imageServer($r['path']).",
+				    '".$r['big_name']."',
+				    ".$r['big_x'].",
+				    ".$r['big_y'].",
+				    ".$r['big_size'].",
+				    '".$r['small_name']."',
+				    ".$r['small_x'].",
+				    ".$r['small_y'].",
+				    ".$r['small_size'].",
+
+					".$r['sort'].",
+					"._comtexUserId($r).",
+					'".$r['dtime_add']."'
+				)";
+		$ASS[$zayav_id][] = query_id($sql);
+	}
+
+	$mass = array();
+	foreach($ASS as $zayav_id => $ids)
+		$mass[] = "(".$zayav_id.",'".implode(',', $ids)."')";
+
+	$sql = "INSERT INTO `_spisok` (
+				`id`,
+				`txt_1`
+			) VALUES ".implode(',', $mass)."
+			ON DUPLICATE KEY UPDATE
+				`txt_1`=VALUES(`txt_1`)";
+	query($sql);
+}
+function _kupez_zayav_poz() {//заявки-поздравления
+	$dialog_id = _comtexSpisokClear(1487);
+
+	_db2();
+	$sql = "SELECT *
+			FROM _zayav
+			WHERE `app_id`=".APP_ID_OLD."
+			  AND `service_id`=10
+			ORDER BY `id`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	$mass = array();
+	foreach($arr as $zayav_id => $r) {
+		$mass[] = "(
+				".$zayav_id.",
+				".APP_ID.",
+				".$r['nomer'].",
+				".$dialog_id.",
+				
+				"._comtexAss(1040, $r['client_id']).",/* клиент */
+
+				".$r['sum_cost'].",
+
+				"._comtexUserId($r).",
+				'".$r['dtime_add']."',
+				".$r['deleted']."
+			)";
+	}
+
+	$sql = "INSERT INTO `_spisok` (
+				  `id_old`,
+				  `app_id`,
+				  `num`,
+				  `dialog_id`,
+				
+				  num_1,
+
+				  sum_1,
+
+				  user_id_add,
+				  dtime_add,
+				  deleted
+			) VALUES ".implode(',', $mass);
+	query($sql);
+}
+function _kupez_zayav_poz_image() {//изображения к рекламе
+	$sql = "UPDATE `_spisok`
+			SET `txt_1`=''
+			WHERE `dialog_id`=1487";
+	query($sql);
+
+	_db2();
+	$sql = "SELECT *
+			FROM `_image`
+			WHERE `app_id`=".APP_ID_OLD."
+			  AND !`deleted`
+			  AND `unit_name`='zayav'
+			  AND `unit_id` IN (
+			    SELECT `id` FROM `_zayav` WHERE `service_id`=10
+			  )
+			ORDER BY `unit_id`,`sort`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	$ASS = array();
+	foreach($arr as $r) {
+		if(!$zayav_id = _comtexAss(1487, $r['unit_id']))
+			continue;
+		if(!isset($ASS[$zayav_id]))
+			$ASS[$zayav_id] = array();
+
+		$sql = "INSERT INTO `_image` (
+					`app_id`,
+	
+					server_id,
+					max_name,
+					max_x,
+					max_y,
+					max_size,
+					80_name,
+					80_x,
+					80_y,
+					80_size,
+	
+					sort,
+					user_id_add,
+					dtime_add
+				) VALUES (
+					".APP_ID.",
+
+					"._imageServer($r['path']).",
+				    '".$r['big_name']."',
+				    ".$r['big_x'].",
+				    ".$r['big_y'].",
+				    ".$r['big_size'].",
+				    '".$r['small_name']."',
+				    ".$r['small_x'].",
+				    ".$r['small_y'].",
+				    ".$r['small_size'].",
+
+					".$r['sort'].",
+					"._comtexUserId($r).",
+					'".$r['dtime_add']."'
+				)";
+		$ASS[$zayav_id][] = query_id($sql);
+	}
+
+	$mass = array();
+	foreach($ASS as $zayav_id => $ids)
+		$mass[] = "(".$zayav_id.",'".implode(',', $ids)."')";
+
+	$sql = "INSERT INTO `_spisok` (
+				`id`,
+				`txt_1`
+			) VALUES ".implode(',', $mass)."
+			ON DUPLICATE KEY UPDATE
+				`txt_1`=VALUES(`txt_1`)";
+	query($sql);
+}
 
 function _kupez_invoice() {//Расчётные счета
 	$dialog_id = _comtexSpisokClear(1483);
@@ -358,6 +621,108 @@ function _kupez_invoice() {//Расчётные счета
 				  sum_1,/* start */
 
 				  sort,
+				  deleted
+			) VALUES ".implode(',', $mass);
+	query($sql);
+}
+function _kupez_invoice_transfer() {//переводы между счетами
+	$dialog_id = _comtexSpisokClear(1488);
+
+	_db2();
+	$sql = "SELECT *
+			FROM _money_invoice_transfer
+			WHERE `app_id`=".APP_ID_OLD."
+			ORDER BY `id`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	$mass = array();
+	foreach($arr as $id => $r) {
+		$mass[] = "(
+				".$id.",
+				".APP_ID.",
+				".$id.",
+				".$dialog_id.",
+
+				"._comtexAss(1483, $r['invoice_id_from']).", /* расчётные счета */
+				"._comtexAss(1483, $r['invoice_id_to']).", /* расчётные счета */
+				".$r['sum'].",
+				'".$r['about']."',
+
+				"._comtexUserId($r).",
+				'".$r['dtime_add']."',
+				".$r['deleted']."
+		)";
+	}
+
+	$sql = "INSERT INTO `_spisok` (
+				  `id_old`,
+				  `app_id`,
+				  `num`,
+				  `dialog_id`,
+				
+				  num_1,
+				  num_2,
+				  sum_1,
+				  txt_1,
+
+				  user_id_add,
+				  dtime_add,
+				  deleted
+			) VALUES ".implode(',', $mass);
+	query($sql);
+}
+function _kupez_invoice_in_out() {//внесения и выводы (перенос в переводы)
+	$dialog_id = 1488;
+
+	_db2();
+	$sql = "SELECT *
+			FROM _money_invoice_out
+			WHERE `app_id`=".APP_ID_OLD."
+			ORDER BY `id`";
+	if(!$arr = query_arr($sql))
+		return;
+
+	//получение id счёта "На телефон Юре"
+	$sql = "SELECT `id`
+			FROM `_spisok`
+			WHERE `dialog_id`=1483
+			  AND `txt_1`='На телефон Юре'";
+	if(!$inTo = query_value($sql))
+		echo 'Не получен id счёта "На телефон Юре"';
+
+	$mass = array();
+	foreach($arr as $id => $r) {
+		$mass[] = "(
+				".$id.",
+				".APP_ID.",
+				".$id.",
+				".$dialog_id.",
+
+				"._comtexAss(1483, $r['invoice_id']).", /* c расчётного счета */
+				".$inTo.", /* на расчётный счёт */
+				".$r['sum'].",
+				'".$r['about']."',
+
+				"._comtexUserId($r).",
+				'".$r['dtime_add']."',
+				".$r['deleted']."
+		)";
+	}
+
+	$sql = "INSERT INTO `_spisok` (
+				  `id_old`,
+				  `app_id`,
+				  `num`,
+				  `dialog_id`,
+				
+				  num_1,
+				  num_2,
+				  sum_1,
+				  txt_1,
+
+				  user_id_add,
+				  dtime_add,
 				  deleted
 			) VALUES ".implode(',', $mass);
 	query($sql);
