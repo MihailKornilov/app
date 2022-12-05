abccf673ff94011627bfc63f3dcd787855e60122
mihan_k <mihan_k@mail.ru>
2022-12-05 17:32:19 +0300

    2022-12-05:1 dump base fix

--- modul/db/db_dump.php
+++ modul/db/db_dump.php
@@ -13,12 +13,16 @@ if(@$_GET['key'] != 'jern32n32Md93J83hs')
 
 
 set_time_limit(300);
-
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 ini_set('display_startup_errors', true);
 
+
 require_once '../../modul/global/global.php';
+
+
+define('BR', '<br>');
+define('MAIL_FROM', 'app@gim-system.ru');
 
 //название запакованного файла
 define('DUMP_NAME', MYSQLI_DATABASE.'_'.strftime('%Y-%m-%d_%H-%M-%S').'.sql.gz');
@@ -27,45 +31,61 @@ define('DUMP_FILE', APP_PATH.'/.tmp/'.DUMP_NAME);
 
 
 
+
+
+
+//                          ---=== Создание дампа базы ===---
 $cmd = '/usr/bin/mysqldump --password='.MYSQLI_PASS.
 						 ' --user='.MYSQLI_USER.
 						 ' --host='.MYSQLI_HOST.
 						 ' '.MYSQLI_DATABASE.' | gzip -c > '.DUMP_FILE.' 2>&1';
 exec($cmd, $out);
+echo 'Archive name: '.DUMP_NAME.BR;
 
 
 
-//чтение содержания архива
+//                          ---=== Получение размера файла ===---
 $file = fopen(DUMP_FILE, 'r');
-$size = filesize(DUMP_FILE);//получение размера файла
+$size = filesize(DUMP_FILE);
 $text = fread($file, $size);
 fclose($file);
+echo 'Size: '._sumSpace($size).BR;
 
-$from = 'global_n@dump';
-$subject = MYSQLI_DATABASE.' - дамп базы '.FullData(curTime()); //Тема писма
-$boundary = '---'; //Разделитель
 
-$headers = "From: $from\nReply-To: $from\n".
-		   'Content-Type: multipart/mixed; boundary="'.$boundary.'"';
-$body =
-	"--$boundary\n".
-	"Content-type: text/html; charset='windows-1251'\n".
-	"Content-Transfer-Encoding: quoted-printablenn".
-	"Content-Disposition: attachment;filename==?windows-1251?B?".base64_encode(DUMP_NAME)."?=\n\n".
+//                          ---=== Подключение PHPMailer ===---
+require '../../inc/PHPMailer/src/PHPMailer.php';
+require '../../inc/PHPMailer/src/Exception.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-	//текст сообщения
-	"Size: "._sumSpace($size)." bytes.\n".
-	"Time: ".round(microtime(true) - TIME, 3)."\n".
 
-	"--$boundary\n".
-	"Content-Type: application/octet-stream;name==?windows-1251?B?".base64_encode(DUMP_NAME)."?=\n".
-	"Content-Transfer-Encoding: base64\n".
-	"Content-Disposition: attachment;filename==?windows-1251?B?".base64_encode(DUMP_NAME)."?=\n\n".
-	chunk_split(base64_encode($text))."\n".
-	'--'.$boundary ."--\n";
 
-if(mail(CRON_MAIL, $subject, $body, $headers))
-	unlink(DUMP_FILE);
 
+
+//                          ---=== Отправка письма ===---
+$mail = new PHPMailer(false);
+
+try {
+    $mail->setFrom(MAIL_FROM, 'global_n');
+    $mail->addAddress(CRON_MAIL);   //адрес получателя
+
+    $mail->addAttachment(DUMP_FILE);
+
+    $mail->isHTML(false);
+    $mail->Subject = MYSQLI_DATABASE.' - base dump '.strftime('%Y-%m-%d');  //заголовок
+    $mail->Body    = DUMP_NAME."\n".
+                    "Size: "._sumSpace($size)." bytes.\n".
+                    "Time: ".round(microtime(true) - TIME, 3)."\n";
+
+    $mail->send();
+
+    unlink(DUMP_FILE);
+    echo BR.'Sent OK.';
+} catch (Exception $e) {
+    mail(CRON_MAIL, 'BASE DUMP ERROR',
+        "Mailer Error: {$mail->ErrorInfo}",
+        "Content-type:text/html; Charset=utf-8\r\nFrom:".MAIL_FROM."\r\n");
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 
